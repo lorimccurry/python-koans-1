3e7f16872c2e605740a4fa149a4b04c66edca034
Lori McCurry <lori.mccurry@gmail.com>
2021-11-24 14:44:02 -0600

    triangle.py part 1

--- koans/triangle.py
+++ koans/triangle.py
@@ -18,7 +18,14 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    triangle_set = set({a,b,c})
+
+    if len(triangle_set) == 1:
+        return 'equilateral'
+    elif len(triangle_set) == 2:
+        return 'isosceles'
+    else:
+        return 'scalene'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
