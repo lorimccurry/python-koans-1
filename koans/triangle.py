32af093402c2ad8a2d09fd2eb941a552da584096
Lori McCurry <lori.mccurry@gmail.com>
2021-11-29 17:33:38 -0600

    about_triangle_project2

--- koans/triangle.py
+++ koans/triangle.py
@@ -17,8 +17,13 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    triangle_set = set({a,b,c})
+    triangle_list = sorted([a, b, c])
+    triangle_set = set(triangle_list)
+    if triangle_list[0] <= 0:
+        raise TriangleError('All sides of a triangle should be greater than 0.')
+
+    if triangle_list[2] >= triangle_list[0] + triangle_list[1]:
+        raise TriangleError('The sum of any two sides should be greater than the third one.')
 
     if len(triangle_set) == 1:
         return 'equilateral'
