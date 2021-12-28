bd8744367761ef694db1ba34f9df46621fd84c5d
Lori McCurry <lori.mccurry@gmail.com>
2021-12-27 18:10:44 -0600

    Game implementation and testing

--- koans/about_extra_credit.py
+++ koans/about_extra_credit.py
@@ -10,6 +10,8 @@
 # Write a player class and a Game class to complete the project.  This
 # is a free form assignment, so approach it however you desire.
 
+from typing import List
+from operator import attrgetter
 from runner.koan import *
 from koans.about_scoring_project import score
 from koans.about_dice_project import DiceSet
@@ -30,28 +32,21 @@ from koans.about_dice_project import DiceSet
     # OK - a player can choose to roll again after each roll unless
         # it is a 0 point roll
 
-    # start
+    # OK - start
     # if no points, then must roll 300 min to get started
 
-    # end
+    # OK end
     # 3000+ points sends to final round
 
-    # final round
-        # one more roll
-        # highest score after roll wins
-
-class Game:
-    # game has num of players
-
-    # game should know when time for final round (3000+ points)
-    pass
-
+    # OK - final round
+        # OK - one more roll
+        # OK - highest score after roll wins
 class Turn:
     def __init__(self):
         self._dice = DiceSet()
         self._active_dice = []
         self._current_score = 0
-        self._turn_over = False
+        self._turn_over = True
 
     @property
     def active_dice(self):
@@ -142,6 +137,7 @@ class Player(Turn):
     def __init__(self, name):
         self._name = name
         self._total_points = 0
+        self._is_winner = False
         super().__init__()
 
     @property
@@ -156,16 +152,96 @@ class Player(Turn):
     def total_points(self, new_total):
         self._total_points = new_total
 
+    @property
+    def is_winner(self):
+        return self._is_winner
+
+    @is_winner.setter
+    def is_winner(self, new_boolean: bool):
+        self._is_winner = new_boolean
+
     def end_turn(self):
         if self.total_points + self.current_score < 300:
             raise ScoreError('You need a minimum of 300 points to end your turn.')
         self.total_points += self.current_score
         self.clean_up_after_turn()
 
+class Game():
+    def __init__(self):
+        self._players = []
+        self._active_player_index = None
+        self._final_round = False
+        self._final_round_turns = 0
+        self._game_over = False
+
+    @property
+    def players(self):
+        return self._players
+
+    @property
+    def active_player_index(self):
+        return self._active_player_index
+
+    @active_player_index.setter
+    def active_player_index(self, new_index):
+        self._active_player_index = new_index
+
+    @property
+    def final_round(self):
+        return self._final_round
+
+    @final_round.setter
+    def final_round(self, is_final):
+        self._final_round = is_final
+
+    def add_player(self, name):
+        self._players.append(Player(name))
+
+    def new_turn(self):
+        if self._game_over == True:
+            raise GameError('The game is over!')
+
+        if self.active_player_index == None:
+            self.active_player_index = 0
+        elif self.active_player_index + 1 >= len(self.players):
+            self.active_player_index = 0
+        else:
+            self.active_player_index += 1
+
+        player_has_3000 = len([player for player in self.players if player.total_points >= 3000]) == 1
+
+        if player_has_3000:
+            self.final_round = True
+
+        if self.final_round == True and self.players[self.active_player_index].total_points < 3000:
+            self._final_round_turns += 1
+
+        # allows player to roll
+        self.players[self.active_player_index].turn_over = False
+
+        if self._final_round_turns >= len(self.players) - 1:
+            self._game_over = True
+
+    def set_winner(self):
+        if self._game_over == False:
+            raise GameError('There can only be a winner when the game is over!')
+        winning_player = max(self.players, key=attrgetter('total_points'))
+        winning_player.is_winner = True
+
+    # game should know when time for final round (3000+ points)
+    # game needs to check players scores at the start of each turn
+    # prop on player used_final_turn
+    # bail if used_final_turn and final_round
+    # game_over function
+        # player.winner = True
+
 class TurnError(Exception):
     pass
 
 class ScoreError(Exception):
+    pass
+
+class GameError(Exception):
     pass
 
 class AboutExtraCredit(Koan):
@@ -174,6 +250,7 @@ class AboutExtraCredit(Koan):
 
     def test_score_of_an_empty_list_is_zero(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = []
         turn.calculate_score()
         self.assertEqual(0, turn.current_score)
@@ -181,6 +258,7 @@ class AboutExtraCredit(Koan):
 
     def test_score_of_a_single_roll_of_5_is_50(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [5]
         turn.calculate_score()
         self.assertEqual(50, turn.current_score)
@@ -188,6 +266,7 @@ class AboutExtraCredit(Koan):
 
     def test_score_of_a_single_roll_of_1_is_100(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [1]
         turn.calculate_score()
         self.assertEqual(100, turn.current_score)
@@ -195,6 +274,7 @@ class AboutExtraCredit(Koan):
 
     def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [1,5,5,1]
         turn.calculate_score()
         self.assertEqual(300, turn.current_score)
@@ -202,6 +282,7 @@ class AboutExtraCredit(Koan):
 
     def test_score_of_single_2s_3s_4s_and_6s_are_zero(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [2,3,4,6]
         turn.calculate_score()
         self.assertEqual(0, turn.current_score)
@@ -209,6 +290,7 @@ class AboutExtraCredit(Koan):
 
     def test_score_of_a_triple_1_is_1000(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [1,1,1]
         turn.calculate_score()
         self.assertEqual(1000, turn.current_score)
@@ -216,6 +298,7 @@ class AboutExtraCredit(Koan):
 
     def test_score_of_other_triples_is_100x(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [2,2,2]
         turn.calculate_score()
         self.assertEqual(200, turn.current_score)
@@ -243,6 +326,7 @@ class AboutExtraCredit(Koan):
 
     def test_score_of_mixed_is_sum(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [2,5,2,2,3]
         turn.calculate_score()
         self.assertEqual(250, turn.current_score)
@@ -260,6 +344,7 @@ class AboutExtraCredit(Koan):
 
     def test_ones_not_left_out(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [1,2,2,2]
         turn.calculate_score()
         self.assertEqual(300, turn.current_score)
@@ -272,11 +357,13 @@ class AboutExtraCredit(Koan):
 
     def test_first_roll_returns_5_dice(self):
         turn = Turn()
+        turn.turn_over = False
         turn.roll()
         self.assertEqual(5, len(turn.active_dice))
 
     def test_a_roll_removes_scoring_dice_from_next_roll(self):
         turn = Turn()
+        turn.turn_over = False
         # first roll
         turn.active_dice = [1,1,5,3,4]
         turn.calculate_score()
@@ -288,30 +375,35 @@ class AboutExtraCredit(Koan):
 
     def test_turn_ends_when_no_scoring_dice(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [3,3,4]
         turn.calculate_score()
         self.assertEqual(True, turn.turn_over)
 
     def test_non_scoring_roll_zeroes_current_turn_score(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [3,3,4]
         turn.calculate_score()
         self.assertEqual(0, turn.current_score)
 
     def test_non_scoring_roll_empties_dice_in_hand(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [3,3,4]
         turn.calculate_score()
         self.assertEqual([], turn.active_dice)
 
     def test_turn_does_not_end_with_scoring_dice(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [1,1,5,3,4]
         turn.calculate_score()
         self.assertEqual(False, turn.turn_over)
 
     def test_depleting_available_dice_with_scoring_roll_will_replenish_five_dice(self):
         turn = Turn()
+        turn.turn_over = False
         turn.active_dice = [5,5,5,5]
         turn.calculate_score()
         self.assertEqual([], turn.active_dice)
@@ -321,7 +413,6 @@ class AboutExtraCredit(Koan):
 
     def test_no_roll_or_calc_score_when_not_turn(self):
         turn = Turn()
-        turn.turn_over = True
         with self.assertRaises(TurnError):
             turn.roll()
         with self.assertRaises(TurnError):
@@ -331,10 +422,11 @@ class AboutExtraCredit(Koan):
         player = Player('player 1')
         self.assertEqual(player.name, 'player 1')
         self.assertEqual(player.total_points, 0)
-        self.assertEqual(player.turn_over, False)
+        self.assertEqual(player.turn_over, True)
 
     def test_ending_player_turn_cleans_up_and_banks_current_score(self):
         player = Player('one')
+        player.turn_over = False
         player.active_dice = [1,1,1,5,3]
         player.calculate_score()
         player.end_turn()
@@ -345,6 +437,7 @@ class AboutExtraCredit(Koan):
 
     def test_player_non_scoring_roll_ends_turn_and_total_score_does_not_change(self):
         player = Player('one')
+        player.turn_over = False
         player._total_points = 500
         player.active_dice = [3,3,4,4]
         player.calculate_score()
@@ -355,6 +448,7 @@ class AboutExtraCredit(Koan):
 
     def test_minimum_initial_score_of_300(self):
         player = Player('one')
+        player.turn_over = False
         player.active_dice = [1,1,3,4,5]
         player.calculate_score()
         with self.assertRaises(ScoreError):
@@ -363,3 +457,105 @@ class AboutExtraCredit(Koan):
         self.assertEqual(False, player.turn_over)
         self.assertEqual([3,4], player.active_dice)
         self.assertEqual(250, player.current_score)
+
+    def test_new_game_has_empty_players_list(self):
+        game = Game()
+        self.assertEqual([], game.players)
+
+    def test_adding_players(self):
+        game = Game()
+
+        game.add_player('player 1')
+        self.assertEqual(1, len(game.players))
+        self.assertEqual('player 1', game.players[0].name)
+
+        game.add_player('player 2')
+        self.assertEqual(2, len(game.players))
+        self.assertEqual('player 2', game.players[1].name)
+
+    def test_new_turn_activates_player_and_checks_for_final_round(self):
+        game = Game()
+        game.add_player('player 1')
+        game.add_player('player 2')
+        self.assertEqual(True, game.players[0].turn_over)
+        self.assertEqual(True, game.players[1].turn_over)
+
+        game.new_turn()
+        self.assertEqual(False, game.players[0].turn_over)
+        self.assertEqual(True, game.players[1].turn_over)
+        self.assertEqual(0, game.active_player_index)
+        self.assertEqual(False, game.final_round)
+
+    def test_final_round_initiation_gives_players_one_more_turn(self):
+        game = Game()
+        game.add_player('player 1')
+        game.add_player('player 2')
+        game.active_player_index = 1
+
+        game.players[0].total_points = 2800
+        game.players[1].total_points = 3000
+
+        game.new_turn()
+
+        self.assertEqual(0, game.active_player_index)
+        self.assertEqual(True, game.final_round)
+        self.assertEqual(1, game._final_round_turns)
+        self.assertEqual(True, game._game_over)
+
+    def test_end_of_final_round_ends_the_game(self):
+        game = Game()
+        game.add_player('player 1')
+        game.add_player('player 2')
+        game.add_player('player 3')
+        game.active_player_index = 1
+
+        game.players[0].total_points = 2800
+        game.players[1].total_points = 3000
+        game.players[2].total_points = 2400
+
+        # first player after 3000 is hit
+        game.new_turn()
+        self.assertEqual(2, game.active_player_index)
+        self.assertEqual(1, game._final_round_turns)
+        game.players[2].turn_over = True
+
+        # last roll for trailing player
+        game.new_turn()
+        self.assertEqual(0, game.active_player_index)
+        self.assertEqual(2, game._final_round_turns)
+        self.assertEqual(True, game._game_over)
+
+    def test_new_turn_raises_error_when_game_over(self):
+        game = Game()
+        game.add_player('player 1')
+        game.add_player('player 2')
+
+        game.players[0].total_points = 2800
+        game.players[1].total_points = 3000
+
+        game._game_over = True
+
+        with self.assertRaises(GameError):
+            game.new_turn()
+
+    def test_winner_set_when_game_over(self):
+        game = Game()
+        game.add_player('player 1')
+        game.add_player('player 2')
+        game.add_player('player 3')
+        game.active_player_index = 1
+
+        game.players[0].total_points = 2800
+        game.players[1].total_points = 3000
+        game.players[2].total_points = 2400
+
+        self.assertEqual(False, game.players[0].is_winner)
+        self.assertEqual(False, game.players[1].is_winner)
+        self.assertEqual(False, game.players[2].is_winner)
+
+        game._game_over = True
+        game.set_winner()
+
+        self.assertEqual(False, game.players[0].is_winner)
+        self.assertEqual(True, game.players[1].is_winner)
+        self.assertEqual(False, game.players[2].is_winner)
